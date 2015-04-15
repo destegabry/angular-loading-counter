20393274964d34e44930c28c89d299a361f4418d
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2015-04-15 16:20:55 +0200

    Created documentation branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,13 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-ngdocs');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     ngdocs: {
+      options: {
+        dest: '.'
+      },
       api: {
         src: ['src/*.js', 'resources/docs/index.ngdoc'],
         title: 'angular-loading-counter'
@@ -28,32 +29,14 @@ module.exports = function (grunt) {
     	},
       beforeconcat: ['./src/**/*.js']
     },
-    concat: {
-      dist: {
-        src: ['src/**/*.js'],
-        dest: 'dist/<%= pkg.namelower %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        mangle: false
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.namelower %>.min.js': ['dist/<%= pkg.namelower %>.js']
-        }
-      }
-    },
-    clean: ['docs', 'dist']
+    clean: ['css', 'font', 'js', 'partials', 'index.html']
   });
 
-  grunt.registerTask('build', [
+  grunt.registerTask('website', [
   	'clean',
     'jshint',
-    'ngdocs',
-    'concat',
-    'uglify'
+    'ngdocs'
   ]);
-  grunt.registerTask('default', ['build']);
+  grunt.registerTask('default', ['website']);
 
 };
---
+++ js/docs-setup.js
@@ -0,0 +1,61 @@
+NG_DOCS={
+  "sections": {
+    "api": "angular-loading-counter"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "angular-loading-counter",
+      "type": "overview",
+      "moduleName": "angular-loading-counter",
+      "shortDescription": "angular-loading-counter",
+      "keywords": "$httpprovider add addexclusion advanced angular angular-loading-counter angularjs api app application class config configure consuming count counted directive dsg exclude exclusion feedback finish hidden hidden-class html http inject interceptors loading loading-counter loadingcounter loadingcounterprovider module ng-app overview paths pending-tasks-count performed push quick service spinner start starting tasks time urls usage var visual yourangularapp"
+    },
+    {
+      "section": "api",
+      "id": "dsg.loadingCounter",
+      "shortName": "dsg.loadingCounter",
+      "type": "overview",
+      "moduleName": "dsg.loadingCounter",
+      "shortDescription": "dsg.loadingCounter",
+      "keywords": "angular api app destefanis dsg gabriele holding http inject license loadingcounter mit module opensource org overview quickstart v1 var yourangularapp"
+    },
+    {
+      "section": "api",
+      "id": "dsg.loadingCounter.directive:loadingCounter",
+      "shortName": "loadingCounter",
+      "type": "directive",
+      "moduleName": "dsg.loadingCounter",
+      "shortDescription": "Directive which hides a DOM element until the LoadingCounter service",
+      "keywords": "aec api attributes bar binding binds changing class countdown css currently directive display dom dsg element grater hidden hiddenclass hides hiding loadingcounter number pending pendingrequestscount pendingtaskscount performed progress property service shownclass sort taks"
+    },
+    {
+      "section": "api",
+      "id": "dsg.loadingCounter.LoadingCounter",
+      "shortName": "dsg.loadingCounter.LoadingCounter",
+      "type": "service",
+      "moduleName": "dsg.loadingCounter",
+      "shortDescription": "LoadingCounter",
+      "keywords": "$q api async background consuming counting dsg loadingcounter number requests service tasks time working"
+    },
+    {
+      "section": "api",
+      "id": "dsg.loadingCounter.LoadingCounterProvider",
+      "shortName": "dsg.loadingCounter.LoadingCounterProvider",
+      "type": "service",
+      "moduleName": "dsg.loadingCounter",
+      "shortDescription": "LoadingCounterProvider",
+      "keywords": "api behaviour configure default dsg loadingcounter loadingcounterprovider service"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
